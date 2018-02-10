ed2ff61575d3ef6a7307e45edefb69912f28caca
sassha96 <syvoplias76@gmail.com>
2018-02-10 06:20:23 +0200

    task2 done :+1:

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,35 +1,64 @@
 let decision = confirm("Do you want to play a game?")
 
-if (decision == false) {
+if (decision === false) {
     console.log("You did not become a millionaire")
 }
 else {
-    let num = Math.floor(Math.random() * 6);
-    let attemps = 3;
+    let rangeMultiplicator = 5;
+    let moneyMultiplicator = 1;
     let userChoice;
     let totalPrize = 0;
-    let currentPrize = 10;
-    for (let i = 1; i < 4; i++) {
-        userChoice = prompt("Enter a number from 0 to 5\nAttempts left : " + attemps + "\nTotal prize: " + totalPrize + "\nPossible prize on current attempt: " + currentPrize, "");
-        if (userChoice != num) {
-            attemps -= 1;
+    let num = Math.floor(Math.random() * (rangeMultiplicator + 1));
+    attemps = 3;
+    let currentPrize;
+    for (let i = 0; i < 3; i++) {
+        currentPrize = 10 * moneyMultiplicator;
+        switch (attemps) {
+            case 2: currentPrize = parseInt(10 / 2 * moneyMultiplicator); break;
+            case 1: currentPrize = parseInt(5 / 2 * moneyMultiplicator); break;
+        }
+        userChoice = prompt("Enter a number from 0 to " + rangeMultiplicator + "\nAttempts left : " + attemps + "\nTotal prize: " + totalPrize + "\nPossible prize on current attempt: " + currentPrize, "0");
+        if (userChoice == num) {
             switch (attemps) {
-                case 2: currentPrize = 5; break;
-                case 1: currentPrize = 2; break;
+                case 3: totalPrize += 10 * moneyMultiplicator; break;
+                case 2: totalPrize += parseInt(10 / 5 * moneyMultiplicator); break;
+                case 1: totalPrize += parseInt(5 / 2 * moneyMultiplicator); break;
             }
-
-
+            decision = confirm("Congrats, do you want to continue the game?");
+            if (decision == false) {
+                console.log("Thank you for a game. Your prize is:" + totalPrize);
+                decision = confirm("Do you wanna play again?");
+                if (decision == false) {
+                    break;
+                } else {
+                    totalPrize = 0;
+                    i = -1;
+                    rangeMultiplicator = 5;
+                    moneyMultiplicator = 1;
+                    attemps = 3;
+                    continue;
+                }
+            } else {
+                i = -1;
+                attemps = 3;
+                rangeMultiplicator *= 2;
+                moneyMultiplicator *= 3;
+                continue;
+            }
         } else {
-            switch (attemps) {
-                case 1: totalPrize += 2; break;
-                case 2: totalPrize += 5; break;
-                case 3: totalPrize += 10; break;
+            attemps -= 1;
+            if (attemps == 0) {
+                console.log("Thank you for a game. Your prize is: " + totalPrize);
+                decision = confirm("Do you want to play again?");
+                if (decision == true) {
+                    totalPrize = 0;
+                    i = -1;
+                    rangeMultiplicator = 5;
+                    moneyMultiplicator = 1;
+                    attemps = 3;
+                    continue;
+                } else { break; }
             }
-            alert("Congrats!");
-            break;
         }
     }
-    alert("Total prize is: " + totalPrize);
-    confirm("Do you want to continue the game?");
-
 }
