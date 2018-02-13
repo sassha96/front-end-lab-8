626bd9e7141748329078d0df772a286f5d4a4fa8
sassha96 <syvoplias76@gmail.com>
2018-02-13 23:01:13 +0200

    5 task fixed

--- homework_08/js/getClosestToZero.js
+++ homework_08/js/getClosestToZero.js
@@ -1,7 +1,7 @@
 function getClosestToZero() {
     let temp = Math.abs(arguments[0]);
     for (let i = 0; i < arguments.length - 1; i++) {
-        if (Math.abs(arguments[i + 1]) < temp) {
+        if (Math.abs(arguments[i] + 1) < temp || arguments[i + 1] === 0) {
             temp = arguments[i + 1];
         }
     }
