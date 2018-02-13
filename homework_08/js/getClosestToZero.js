420c2a9b6587391fe96eee8f32ca8ef113162fa9
sassha96 <syvoplias76@gmail.com>
2018-02-13 22:44:43 +0200

    replaced getMin to getMin.js

--- homework_08/js/getClosestToZero.js
+++
@@ -1,7 +0,0 @@
-function getMin() {
-    let min = arguments[0];
-    for (let i = 0; i < arguments.length - 2; i++) {
-        if (arguments[i + 1] < arguments[i + 2]) { min = arguments[i + 1]; }
-    }
-    return min;
-}
